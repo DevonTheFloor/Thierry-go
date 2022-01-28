640213f5c2f5e3e2a620c162e26f5d580ffbf764
DevonTheFloor <devonthefloor@free.fr>
2022-01-28 19:17:50 +0100

    Created MyHeader

---
+++ components/MyHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="urlim">
+    <h1>
+      <slot name="titre"></slot>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+  props: {
+    urlim: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
