6f21d2ab497a6a75543e04fce70c51e58c712e1a
DevonTheFloor <devonthefloor@free.fr>
2022-01-29 09:28:00 +0100

    Add Stack

---
+++ components/Stack.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="stack">
+    <figure>
+      <img class="profile" src="~static/profil.jpg">
+    </figure>
+    <h2 class="sub-title">
+      Développeur d'application web et mobile, full stack JS.
+    </h2>
+    <div>
+      <figure>
+        <img src="~@/static/HTML5.png" alt="logo html 5" class="ecusson">
+        <img src="~@/static/css3.png" alt="logo css 3" class="ecusson">
+        <img src="~@/static/JS.png" alt="logo JavaScript" class="ecusson">
+        <img src="~@/static/node.png" alt="logo Node.js" class="ecusson">
+        <img src="~@/static/mongo.png" alt="logo mongodb" class="ecusson">
+        <img src="~@/static/vue.png" alt="logo vue js" class="ecusson">
+        <img src="~@/static/v.png" alt="logo vuetify" class="ecusson">
+      </figure>
+    </div>
+  </div>
+</template>
